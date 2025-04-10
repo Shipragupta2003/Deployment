619e1caf2f163bf0eb1d5f39ae1c24ffc39070e4
Shipra Gupta <shipraagupta1@gmail.com>
2025-04-11 01:07:12 +0530

    latest edit in backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // middleware
 const corsOptions = {
-    origin: "https://deploy-frontend-dl91.onrender.com/" // frontend URI (ReactJS)
+    origin: "https://deploy-frontend-dl91.onrender.com" // frontend URI (ReactJS)
 }
 app.use(express.json());
 app.use(cors(corsOptions));
